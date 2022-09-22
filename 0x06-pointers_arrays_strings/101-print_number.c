9ce867d38fae2492a6c097c4d192f58e16b201f3
DaliaAli606 <daliahamam404@gmail.com>
2022-09-22 04:59:52 -0700

    My first commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,14 +7,14 @@
  */
 int base10(int n)
 {
-    int base = 10;
+	int base = 10;
 
-    while (n > 0)
-	  {
-	      base *= 10;
-	      n--;
-	      }
-	  return (base);
+	while (n > 0)
+	{
+		base *= 10;
+		n--;
+	}
+	return (base);
 }
 
 /**
@@ -24,29 +24,29 @@ int base10(int n)
  */
 void print_number(int n)
 {
-    int power;
+	int power;
 
-    power = base10(8);
+	power = base10(8);
 
-    if (n < 0)
-       {
-	   _putchar('-');
-	   n *= -1;
-	   }
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
 
-       if (n == 0)
-	  _putchar('0');
+	if (n == 0)
+		_putchar('0');
 
-	  else
-	      {
-		  while (n / power == 0)
+	else
+	{
+		while (n / power == 0)
 			power /= 10;
 
-			while (power >= 1)
-			      {
-				  _putchar((n / power) + '0');
-				  n %= power;
-				  power /= 10;
-				  }
-			      }
+		while (power >= 1)
+		{
+			_putchar((n / power) + '0');
+			n %= power;
+			power /= 10;
+		}
+	}
 }

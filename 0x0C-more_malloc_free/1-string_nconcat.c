f9d6b1e685dafd49f7de2423edfeb5fb8d4f7cb8
DaliaAli606 <daliahamam404@gmail.com>
2022-10-06 05:09:54 -0700

    My first commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,24 +30,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
     if (s1 == NULL) /* account for NULL strings */
        s1 = "";
-       if (s2 == NULL)
+    if (s2 == NULL)
 	  s2 = "";
-	  if (num < 0) /* account for negative n bytes */
+    if (num < 0) /* account for negative n bytes */
 	     return (NULL);
-	     if (num >= _strlen(s2)) /* account for n too big */
-		num = _strlen(s2);
+    if (num >= _strlen(s2)) /* account for n too big */
+	    num = _strlen(s2);
 
-		len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
+    len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
 
-		ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
-		if (ptr == NULL)
-		   return (NULL);
+    ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
+    if (ptr == NULL)
+	    return (NULL);
 
-		   for (i = 0; s1[i] != '\0'; i++) /* concat */
-		       ptr[i] = s1[i];
-		       for (j = 0; j < num; j++)
-			   ptr[i + j] = s2[j];
-			   ptr[i + j] = '\0';
+    for (i = 0; s1[i] != '\0'; i++) /* concat */
+	    ptr[i] = s1[i];
+    for (j = 0; j < num; j++)
+	    ptr[i + j] = s2[j];
+    ptr[i + j] = '\0';
 
-			   return (ptr);
+    return (ptr);
 }

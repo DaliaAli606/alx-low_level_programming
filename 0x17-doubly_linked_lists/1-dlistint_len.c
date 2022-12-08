efeb852e0ed9fb185a9e999aaa353925792cd205
DaliaAli606 <daliahamam404@gmail.com>
2022-12-08 10:58:32 -0800

    My first commit

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+#include "lists.h"
+size_t dlistint_len(const dlistint_t *h)
+{
+    size_t i = 0;
+
+    while (h)
+    {
+	  h = h->next;
+	  i++;
+    }
+    return (i);
+}
